7fc629b399187edb2852b50d9b2323ef3681534d
mandinha2108 <amafur8@gmail.com>
2024-05-20 11:00:18 -0300

    configurado muitos para muitos e injeção de dependencia

--- FramaFlix/Data/AppDbContext.cs
+++ FramaFlix/Data/AppDbContext.cs
@@ -1,15 +1,37 @@
 using FramaFlix.Models;
+using Microsoft.AspNetCore.DataProtection.XmlEncryption;
 using Microsoft.EntityFrameworkCore;
 
 namespace FramaFlix.Data;
 
-    public class AppDbContext : DbContext
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-        {
-        }
-
-        public DbSet<Genre> Genres { get; set; }
-        public DbSet<Movie> Movies { get; set; }
-       // public DbSet<MovieGenre> MovieGenres { get; set; }
     }
+
+    public DbSet<Genre> Genres { get; set; }
+    public DbSet<Movie> Movies { get; set; }
+    public DbSet<MovieGenre> MovieGenres { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        #region Configuração de Muitos para Muitos do MoviGenre
+        builder.Entity<MovieGenre>()
+           .HasKey(mg => new { mg.MovieId, mg.GenreId });
+
+        builder.Entity<MovieGenre>()
+           .HasOne(mg => mg.Movie)
+           .WithMany(m => m.Genres)
+           .HasForeignKey(mg => mg.MovieId);
+
+        builder.Entity<MovieGenre>()
+           .HasOne(mg => mg.Genre)
+           .WithMany(g => g.Movies)
+           .HasForeignKey(mg => mg.GenreId);
+        #endregion
+    }
+}
